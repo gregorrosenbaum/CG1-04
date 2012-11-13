07209f2a8062087f08eb240f0ffd0b9aa6107733
Johann <git@johann-hofmann.com>
2012-11-13 11:23:08 +0100

    Das Reh ist gekocht

--- src/ray/Ray.java
+++ src/ray/Ray.java
@@ -1,9 +1,23 @@
 package ray;
 
 import vectorlib.Point3;
+import vectorlib.Vector3;
 
 public class Ray {
 	
-	public Point3 point;
+	public final Point3 o;
+	public final Vector3 d;
+	
+	public Ray(final Point3 o, final Vector3 d){
+		this.o = o;
+		this.d = d;
+	}
 
+	public Point3 at(double t){
+		return o.add(d);
+	}
+	
+	public Vector3 tOf(Point3 p){
+		return p.sub(o);
+	}
 }
