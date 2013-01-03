ec7cbc46055b165df94edf453b9ddf4bfde25112
Anton <Anton@141.64.168.174>
2013-01-03 13:46:24 +0100

    Triangle modified, calculation of Normal3 with Points a,b,c of triangle

--- src/object/Triangle.java
+++ src/object/Triangle.java
@@ -53,15 +53,23 @@ public class Triangle extends Geometry {
 	@Override
 	public Hit hit(Ray r) {
 		Mat3x3 A = new Mat3x3(a.x - b.x, a.x - c.x, r.d.x, a.y - b.y, a.y - c.y, r.d.y, a.z - b.z, a.z - c.z, r.d.z);
-		Vector3 b = new Vector3(a.x - r.o.x, a.y - r.o.y, a.z - r.o.z);
-		Mat3x3 A1 = A.changeCol1(b);
-		Mat3x3 A2 = A.changeCol2(b);
-		Mat3x3 A3 = A.changeCol3(b);
+		Vector3 x = new Vector3(a.x - r.o.x, a.y - r.o.y, a.z - r.o.z);
+		Mat3x3 A1 = A.changeCol1(x);
+		Mat3x3 A2 = A.changeCol2(x);
+		Mat3x3 A3 = A.changeCol3(x);
 		double beta = A1.determinant / A.determinant;
 		double gamma = A2.determinant / A.determinant;
 		double t = A3.determinant / A.determinant;
+//		Normal3 n1 = new Normal3 (0,1,0);
+//		Normal3 n2 = new Normal3 (0,1,0);
+//		Normal3 n3 = new Normal3 (0,1,0);
+		Vector3 v1 = new Vector3 (b.x - a.x, b.y - a.y, b.z - a.z);
+		Vector3 v2 = new Vector3 (c.x - a.x, c.y - a.y, c.z - a.z);
+		Vector3 direction = (v2.x(v1));
+		Normal3 norm = direction.asNormal();
+//		Normal3 norm = new Normal3 (0 ,1 ,0);
 		if (t > Eps.ylon && Eps.ylon <= beta && Eps.ylon <= gamma && beta + gamma <= 1) {
-			return new Hit(t, r, this, new Normal3(0, 1, 0));
+			return new Hit(t, r, this, norm);
 		}
 		return null;
 	}
